c4243b011a94b926fa29be888cf266e9eb96b381
xecua <courser4848@gmail.com>
2019-10-29 01:13:40 +0900

    miss

--- migrations/2019-10-17-143924_update-v2.4.2/down.sql
+++ migrations/2019-10-17-143924_update-v2.4.2/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-DELETE from songs WHERE title IN ("Diode", "FREEF4LL", "GLORY:ROAD");
+DELETE from songs WHERE title IN ("Diode", "FREEF4LL", "GLORY:ROAD", "Monochrome Princess", "Heavenly careless");
