7ba199a6f28977e3a8dc95d6b50ae65fdf890d55
xecua <courser4848@gmail.com>
2019-11-01 18:34:47 +0900

    fix test

--- src/tests.rs
+++ src/tests.rs
@@ -5,6 +5,7 @@ fn test_parse_command() {
     // white box test
     assert!(parse_command("this is not a command").is_none());
     assert!(parse_command("@bot_xecua_odai").is_none());
-    assert_eq!(parse_command("@bot_xecua_odai help").unwrap(), Command::Help);
-    assert_eq!(parse_command("random 1").unwrap(), Command::Random(vec![String::from("1")]));
+    assert!(parse_command("@bot_xecua_odai help").is_none());
+    assert_eq!(parse_command("@bot_xecua_odai /help").unwrap(), Command::Help);
+    assert_eq!(parse_command("/random 1").unwrap(), Command::Random(vec![String::from("1")]));
 }
