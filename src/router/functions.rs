e061561c385135c0d8eb34ef3b0b5d763ee7041d
xecua <courser4848@gmail.com>
2020-01-11 13:28:12 +0900

    ぽ

--- src/router/functions.rs
+++ src/router/functions.rs
@@ -65,6 +65,7 @@ pub const HELP_TEXT: &'static str = r#"## このBotの使い方
 + `/random [difficulty|level]` : 全曲全譜面から適当にお題を出します
   + `difficulty`に難易度(PAST/PST, PRESENT/PRS, FUTURE/FTR)を空白区切りで指定すると、その中からのみ出題します
   + `level`にレベル値(1~9,9+,10)を指定すると、その中からのみ出題します
+## おまけ機能
 + `/stamp [n] [stamp_list]`: n個のスタンプをランダムで召喚します 省略した場合n=1です stamp_listは空白区切りでスタンプ名を置くとそこからn個選択します(ちなみに存在するかはチェックしません) 省略するとtraQから引っ張ってきます
 + /omikuji おみくじ代行サービス代行サービスです
 "#;
